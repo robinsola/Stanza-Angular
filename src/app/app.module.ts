69a2dfb4b3a8986ad1b9048c5d5e7c956a69a6ce
Student <example@example.com>
2018-06-19 15:42:26 -0700

    add splash component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
